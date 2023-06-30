439fe5e68e9bcd09e95f2287e8bd6a46f9348e04
sana <sanahmad15046@gmail.com>
2023-06-30 13:20:07 +0530

    changed the css of body and made the code easy to read

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
 
 function App() {
   const [newTask, setNewTask] = useState('');
@@ -10,7 +7,7 @@ function App() {
 
   const addTask = () => {
     setTodoList([...todoList, newTask]);
-    setNewTask("");// Clear the input field after adding a task
+    setNewTask("");
   };
 
   const handleChange = (event) => {
@@ -22,47 +19,29 @@ function App() {
   };
 
   const deleteIconStyle = {
-    width: '24px', // Adjust the width to the desired size
+    width: '24px', 
     height: '24px',
     cursor: 'pointer',
     marginLeft: '10x',
   };
-
-  const listItemStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '400px',
-    padding: '10px',
-    borderRadius: '15px',
-    marginBottom: '10px',
-    
-  };
-
   
   return (
     <div className="App">
 
-      
-
       <div className="add-task">
-      
         <input className="input" value={newTask} onChange={handleChange} />
-        
         <button className="addtaskbtn" onClick={addTask} >ADD TASK</button>
       </div>
 
       <div className="list">
-        
         {todoList.map((task, index) => (
-          <div key={index} style={listItemStyle}>
+          <div key={index} className="listItemStyle">
             <span style={{ marginRight: '200px' ,fontSize: '26px', textAlign: 'center', flex: 1 , fontFamily: 'Verdana, Geneva, Tahoma, sans-serif' }}>{task}</span>
             <img src="https://cdn1.iconfinder.com/data/icons/color-bold-style/21/56-512.png" alt="x" style={deleteIconStyle} onClick={()=>deleteTask(index)}/>
           </div> 
           ))}
       </div>
-      
-
+    
     </div>
   );
 }
